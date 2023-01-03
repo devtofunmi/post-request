0d837a3c11c29629c313090612644f81feb16563
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-01-03 03:03:26 +0100

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { Box } from "@chakra-ui/react";
+import Homepage from "../pages/Homepage";
 function App() {
-  return <Box>hi</Box>;
+  return (
+    <Box w={"100%"} minH={"100vh"} bg={"black"} color={"white"}>
+      <Homepage />
+    </Box>
+  );
 }
 
 export default App;
