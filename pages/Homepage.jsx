82e8c7b3bcd860fd400eb40168c0f7ccbd40e5e8
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-01-04 04:14:00 +0100

    done

--- pages/Homepage.jsx
+++ pages/Homepage.jsx
@@ -3,12 +3,18 @@ import { Box, Flex, Input, Button, Text } from "@chakra-ui/react";
 import axios from "axios";
 const Homepage = () => {
   const [email, setEmail] = useState("");
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
   function handleSubmit(event) {
     event.preventDefault();
     axios
-      .post("https://fakestoreapi.com/users", {
+      .post("https://api.prettybio.com/auth/signup", {
         email: email,
+        userName: userName,
+        password: password,
+        confirmPassword: confirmPassword,
       })
       .then((response) => console.log(response))
       .catch((error) => console.error(error));
@@ -16,18 +22,49 @@ const Homepage = () => {
 
   return (
     <Box w={["90%", "80%", "40%"]} m="auto" pt={"100px"} textAlign={"center"}>
-      <Text fontSize={"5xl"}>Bring every idea to life</Text>
-      <Text fontSize={"2xl"}>Join waitlist to get notify when we launch</Text>
+      <Text fontSize={"3xl"}>Sign Up</Text>
       <Flex direction={"column"} pt={"20px"} w={"300px"} m="auto">
-        <Input
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-          type="text"
-          placeholder="enter email"
-        />
+        <Box>
+          <Input
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            type="email"
+            placeholder="enter email"
+          />
+        </Box>
+        <Box mt={"20px"}>
+          <Input
+            onChange={(e) => {
+              setUserName(e.target.value);
+            }}
+            type="text"
+            placeholder="enter username"
+          />
+        </Box>
+
+        <Box mt={"20px"}>
+          <Input
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            type="password"
+            placeholder="enter password"
+          />
+        </Box>
+
+        <Box mt={"20px"}>
+          <Input
+            onChange={(e) => {
+              setConfirmPassword(e.target.value);
+            }}
+            type="password"
+            placeholder="confirm password"
+          />
+        </Box>
+
         <Button onClick={handleSubmit} bg={"blue.300"} mt={"10px"}>
-          Join Waitlist
+          Sign Up
         </Button>
       </Flex>
     </Box>
